2d3da71993ec0af343e2f8b036a3f63039bdf1f1
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-04-14 10:37:08 +0200

    Finalized inheritance export class

---
+++ classes/Task.js
@@ -0,0 +1,13 @@
+export class Task {
+  _id;
+  _type;
+  _name;
+  _date;
+
+  constructor(taskId, taskType, taskName, taskDate) {
+    this._id = taskId;
+    this._type = taskType;
+    this._name = taskName;
+    this._date = taskDate;
+  }
+}
